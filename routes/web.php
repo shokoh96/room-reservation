6fe748a06b98e0065e05958bd44f08963256c001
KOH <gaosheng087@gmail.com>
2022-03-17 12:11:19 +0900

    ルート作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::prefix('reservation')->middleware('auth')->group(function () {
+    Route::get('/', [ReservationController::class, 'index'])->name('reservation.index');
+    Route::get('/reservation_list', [ReservationController::class, 'reservation_list'])->name('reservation.reservation_list');
+    Route::post('/', [ReservationController::class, 'store'])->name('reservation.store');
+});
